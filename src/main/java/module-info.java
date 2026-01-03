8ca2a7f8e3cbbc694352329b8d3637455843021b
sanzh <sanzharzhanalinov21@gmail.com>
2026-01-03 10:03:56 +0100

    Add support for session and client creation forms

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,16 +1,8 @@
 module com.example.jp_1 {
     requires javafx.controls;
     requires javafx.fxml;
-    requires javafx.web;
-
-    requires org.controlsfx.controls;
-    requires com.dlsc.formsfx;
-    requires net.synedra.validatorfx;
-    requires org.kordamp.ikonli.javafx;
-    requires org.kordamp.bootstrapfx.core;
-    requires eu.hansolo.tilesfx;
+    requires javafx.base;
     requires java.sql;
-
     opens com.example.jp_1 to javafx.fxml;
     opens com.example.jp_1.model to javafx.base;
     exports com.example.jp_1;
